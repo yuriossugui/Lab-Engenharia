1d31ec2032c419654d6cd57d6b3e13fdc95bfe87
yuriossugui <yuriossugui2@gmail.com>
2025-03-17 21:32:12 -0300

    atividade 04

---
+++ atividade04/script.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const radioProfessor = document.getElementById('tipoProfessor');
+    const radioAluno = document.getElementById('tipoAluno');
+
+    radioAluno.addEventListener('change', function () {
+        let alunoFields = document.getElementById('alunoFields');
+        let professorFields = document.getElementById('professorFields');
+        alunoFields.style.setProperty('display','block');
+        professorFields.style.setProperty('display','none');
+    });
+
+    radioProfessor.addEventListener('change', function () {
+        let alunoFields = document.getElementById('alunoFields');
+        let professorFields = document.getElementById('professorFields');
+        professorFields.style.setProperty('display','block');
+        alunoFields.style.setProperty('display','none');
+    });
+
+
+    class Pessoa{
+        constructor(nome,email,data,telefoneFixo,telefoneCelular,matricula){
+            this.nome = nome;
+            this.email = email;
+            this.data = data;
+            this.telefoneFixo = telefoneFixo;
+            this.telefoneCelular = telefoneCelular;
+            this.matricula = matricula;
+        }
+    }
+
+    class Aluno extends Pessoa{
+        constructor(nome,email,data,telefoneFixo,telefoneCelular,matricula,curso){
+            super(nome,email,data,telefoneFixo,telefoneCelular,matricula)
+            this.curso = curso;
+        }
+    }
+
+    class Professor extends Pessoa{
+        constructor(nome,email,data,telefoneFixo,telefoneCelular,matricula,area,lattes){
+            super(nome,email,data,telefoneFixo,telefoneCelular,matricula)
+            this.area = area;
+            this.lattes = lattes;
+        }
+    }
+
+    const btnSubmit = document.getElementById('btnSubmit');
+    btnSubmit.addEventListener('click', function() {
+
+        let nome = document.getElementById('nome').value;
+        let email = document.getElementById('email').value;
+        let dataNascimento = document.getElementById('dataNascimento').value;
+        let telFixo = document.getElementById('telFixo').value;
+        let telCelular = document.getElementById('telCelular').value;
+
+        let tipo = document.getElementById()
+
+    });
+
+});
